0ceebc44bb167ace104d250d74feaa0be6c02a27
Matheus Ribeiro <mathribe2020@gmail.com>
2022-10-15 19:41:25 -0300

    ci: add storybook deploy workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,22 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+	],
+	framework: '@storybook/react',
+	core: {
+		builder: '@storybook/builder-vite',
+	},
+	features: {
+		storyStoreV7: true,
+	},
+	viteFinal: (config, { configType }) => {
+		if (configType == 'PRODUCTION') {
+			config.base = '/IgniteLab-Design-System/';
+		}
+
+		return config;
+	},
+};
