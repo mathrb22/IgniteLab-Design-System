e0dc2df3ef24e9b3e246009b50e2ba93467d2238
Matheus Ribeiro <mathribe2020@gmail.com>
2022-10-15 21:12:39 -0300

    feat: create the login page using the design system components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,22 @@
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	children: ReactNode;
 	asChild?: boolean;
+	className?: string;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className, ...rest }: ButtonProps) {
 	const Comp = asChild ? Slot : 'button';
 	return (
 		<Comp
 			className={clsx(
-				'py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full hover:bg-cyan-300 transition-colors focus:ring-2 focus:ring-white focus:outline-none'
-			)}>
+				'py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full hover:bg-cyan-300 transition-colors focus:ring-2 focus:ring-white focus:outline-none',
+				className
+			)}
+			{...rest}>
 			{children}
 		</Comp>
 	);
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,17 +6,27 @@ export interface HeadingProps {
 	size?: 'sm' | 'md' | 'lg';
 	children: ReactNode;
 	asChild?: boolean;
+	className?: string;
 }
 
-export function Heading({ size = 'md', children, asChild }: HeadingProps) {
+export function Heading({
+	size = 'md',
+	children,
+	asChild,
+	className,
+}: HeadingProps) {
 	const Comp = asChild ? Slot : 'h2';
 	return (
 		<Comp
-			className={clsx('text-gray-100 font-bold font-sans', {
-				'text-lg': size === 'sm',
-				'text-xl': size === 'md',
-				'text-2xl': size === 'lg',
-			})}>
+			className={clsx(
+				'text-gray-100 font-bold font-sans',
+				{
+					'text-lg': size === 'sm',
+					'text-xl': size === 'md',
+					'text-2xl': size === 'lg',
+				},
+				className
+			)}>
 			{children}
 		</Comp>
 	);
